81025f5c3138331c4ade34074a270fd1111bd120
dnjsgkfka <haribo101@seoultech.ac.kr>
2025-11-24 04:11:49 +0900

    feat: 탭 아이콘 및 이름

--- be-echo-fe/src/index.js
+++ be-echo-fe/src/index.js
@@ -3,6 +3,23 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const setFavicon = () => {
+  const existingLinks = document.querySelectorAll("link[rel*='icon']");
+  existingLinks.forEach((link) => link.remove());
+
+  const link = document.createElement("link");
+  link.type = "image/svg+xml";
+  link.rel = "icon";
+  link.href = "/logo.svg";
+  document.getElementsByTagName("head")[0].appendChild(link);
+
+  if (document.title !== "BeEcho.") {
+    document.title = "BeEcho.";
+  }
+};
+
+setFavicon();
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
